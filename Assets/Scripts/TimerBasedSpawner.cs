6a850208ace28594f6db2fe46d4992907c80bdde
Justin Hamilton <justinanthonyhamilton@gmail.com>
2014-12-19 18:35:27 -0800

    Adding slime spawner

---
+++ Assets/Scripts/TimerBasedSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerBasedSpawner : MonoBehaviour {
+  public int secondsBetweenSpawning = 2;
+  public bool spawn = true;
+  public GameObject slimePrefab;
+
+  private bool spawning = false;
+  private GameObjectPool slimePool;
+  private Vector3 myPosition;
+
+  void Awake() {
+    myPosition = GetComponent<Transform>().position;
+    slimePool = new GameObjectPool(slimePrefab);
+  }
+
+  void FixedUpdate() {
+    if (spawn && !spawning) {
+      StartCoroutine("SpawnByTimer");
+    }
+  }
+
+  private IEnumerator SpawnByTimer() {
+    spawning = true;
+    while (spawn) {
+      yield return new WaitForSeconds(secondsBetweenSpawning);
+      Spawn();
+    }
+  }
+
+  private void Spawn() {
+    slimePool.Get(myPosition);
+  }
+}
