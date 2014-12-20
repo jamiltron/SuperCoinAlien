339baa01dfbe203c34ef6150d2b47c7281255eaa
Justin Hamilton <justinanthonyhamilton@gmail.com>
2014-12-19 23:27:01 -0800

    Adding the start of a lobby system

---
+++ Assets/Scripts/NetworkLobby.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NetworkLobby : MonoBehaviour {
+
+  public ScrollRect scrollRect;
+
+  private enum NetworkState {
+    Starting,
+    JoiningLobby,
+    JoinedLobby
+  }
+
+  private NetworkState networkState;
+  private RoomInfo[] roomInfo;
+   
+
+  void Start() {
+    networkState = NetworkState.JoiningLobby;
+    PhotonNetwork.ConnectUsingSettings("v1.0");
+  }
+
+  void OnFailedToConnectToPhoton() {
+    Debug.LogError("Failed to load photon!");
+  }
+
+  void OnJoinedLobby() {
+    networkState = NetworkState.JoinedLobby;
+    Debug.Log("Lobby joined!");
+  }
+
+  void DisplayLobby() {
+    if (networkState == NetworkState.JoinedLobby) {
+      roomInfo = PhotonNetwork.GetRoomList();
+
+      string roomString = "";
+      foreach (var room in roomInfo) {
+        roomString += room.name + "\n";
+      }
+    }
+  }
+
+}
---
+++ Assets/Scripts/ScrollableList.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ScrollableList : MonoBehaviour {
+  public GameObject roomPrefab;
+
+  
+  
+  void Start() {
+    //RectTransform roomRectTransform = roomPrefab.GetComponent<RectTransform>();
+    RectTransform containerRectTransform = gameObject.GetComponent<RectTransform>();
+        
+    for (int i = 0; i < 1; i++) {
+      GameObject newRoom = Instantiate(roomPrefab) as GameObject;
+      newRoom.name = gameObject.name + i;
+      newRoom.transform.parent = gameObject.transform;
+
+      RectTransform roomTransform = newRoom.GetComponent<RectTransform>();
+      float width = roomTransform.rect.width;
+      float height = roomTransform.rect.height;
+
+      float x = -containerRectTransform.rect.width / 2;
+      float y = containerRectTransform.rect.height / 2 - height * i - height;
+      roomTransform.offsetMin = new Vector2(x, y);
+
+      x = roomTransform.offsetMin.x + width;
+      y = roomTransform.offsetMin.y + height;
+      roomTransform.offsetMax = new Vector2(x, y);
+    }
+    
+    
+  }
+  
+}
